e48f3bf3ba7d5832eb95f221df5d15c1231b61cb
Gabriel Wilkins <wilkigab001@gmail.com>
2023-01-29 21:59:52 -0700

    Added Chart Functionality

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Bar} from 'react-chartjs-2'
+
+const Chart = () => {
+  return (
+    <div>Chart</div>
+  )
+}
+
+export default Chart
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import axios from "axios";
+import AuthContext from "../../store/authContext";
+import Chart from "../Chart/Chart";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [applications, setApplications] = useState([]);
+  const { userId } = useContext(AuthContext);
 
-export default Home
+  const getApplications = () => {
+    axios
+      .get(`/applications/${userId}`)
+      .then((res) => {
+        console.log(res.data);
+        setApplications(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getApplications();
+  }, []);
+
+  return (
+    <div>
+      <Chart data={applications} />
+    </div>
+  );
+};
+
+export default Home;
