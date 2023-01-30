06405adbb9ae1cffb0c1a3eb87a240c9ab7a503c
Gabriel Wilkins <wilkigab001@gmail.com>
2023-01-29 21:29:13 -0700

    Added all front facing functionality except for chart js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div>
+        <p> Website Designed by Gabriel Wilkins in 2023</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext } from "react";
+import AuthContext from "../../store/authContext";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const authCtx = useContext(AuthContext);
 
-export default Header
+  return (
+    <div>
+      {authCtx.token ? (
+        <div>
+          <button onClick={() => authCtx.logout()}>logout</button>
+          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/applicationForm">New Application</NavLink>
+          <NavLink to="/">Home</NavLink>
+        </div>
+      ) : (
+        <div>
+        <NavLink to="/login">Login </NavLink>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
